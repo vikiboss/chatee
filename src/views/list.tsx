cd9fb257b032144fa4a33125bbc5f8da0fe23019
Viki <hi@viki.moe>
2024-07-02 17:07:10 +0800

    feat: add list

--- src/views/list.tsx
+++ src/views/list.tsx
@@ -6,23 +6,28 @@ export function List() {
 		s.friendList,
 		s.groupList,
 	]);
+
 	return (
 		<Box display="flex" flexDirection="column">
-			<Box>
+			<Box display="flex" flexDirection="column">
 				<Text bold>Friends</Text>
 				{friends.map((e) => (
-					<Text key={e.user_id}>
-						{e.nickname} ({e.user_id})
-					</Text>
+					<Box key={e.user_id}>
+						<Text>
+							{e.nickname} ({e.user_id})
+						</Text>
+					</Box>
 				))}
 				{friends.length === 0 && <Text>No friends</Text>}
 			</Box>
-			<Box>
+			<Box display="flex" flexDirection="column">
 				<Text bold>Groups</Text>
 				{groups.map((e) => (
-					<Text key={e.group_id}>
-						{e.group_name} ({e.group_id})
-					</Text>
+					<Box key={e.group_id}>
+						<Text>
+							{e.group_name} ({e.group_id})
+						</Text>
+					</Box>
 				))}
 				{groups.length === 0 && <Text>No groups</Text>}
 			</Box>
--- src/views/qrcode.tsx
+++ src/views/qrcode.tsx
@@ -1,12 +1,27 @@
 import { Text, useInput } from "ink";
 import { client } from "../client";
+import { useIsOnline } from "../hooks/use-is-online";
 
 export function QRcode() {
+	const isOnline = useIsOnline();
+
 	useInput((input, key) => {
 		if (key.return) {
 			client?.login();
 		}
 	});
 
-	return <Text>Scan QRcode, then press enter to continue...</Text>;
+	return (
+		<>
+			{isOnline ? (
+				<Text dimColor color="green">
+					You‘re login! Press `Tab` to view list.
+				</Text>
+			) : (
+				<Text dimColor color="cyan">
+					Scan QRcode, then press enter to continue...
+				</Text>
+			)}
+		</>
+	);
 }
