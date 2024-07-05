ad9c6f130d7e32d8cefeeb8aafb263c7112cc1f7
Viki <hi@viki.moe>
2024-07-05 16:05:35 +0800

    chore: modify build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: ["./src/index.tsx"],
+	splitting: false,
 	format: ["esm"],
 	treeshake: true,
 	target: "node18",
 	platform: "node",
 	dts: false,
+	clean: true,
+	minify: true,
 	noExternal: ["@shined/react-use"],
 });
