7754804bb676016b4ccf4e405b9a6abe3b11b292
Viki <hi@viki.moe>
2024-07-12 20:02:07 +0800

    chore: refine log

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,8 @@
 import path from "node:path";
-import { createClient, Platform, type Client } from "@icqqjs/icqq";
+import { createClient, Platform } from "@icqqjs/icqq";
 import { paths, store } from "./store";
+
+import type { Client } from "@icqqjs/icqq";
 
 const logFilename = `chatee-${new Date().toLocaleString()}.log`;
 const logFilePath = path.join(paths.chateeLogDir, logFilename);
@@ -14,21 +16,6 @@ export const envSignAPI = CHATEE_SIGN_API;
 export const envPlatform = CHATEE_PLATFORM ? +CHATEE_PLATFORM : undefined;
 
 export const defaultSignAPI = "https://qsign.viki.moe/sign";
-
-const buildImage = (hash: string) =>
-	`[https://gchat.qpic.cn/gchatpic_new/0/0-0-${hash}/0?term=2&is_origin=1]`;
-
-const imageReg = /{image:([0-9A-F]{32})}/i;
-
-const replaceImage = (text: string): string => {
-	const match = text.match(imageReg);
-
-	if (match) {
-		return replaceImage(match[1] ? buildImage(match[1]) : text);
-	}
-
-	return text;
-};
 
 export function setupClient() {
 	const { signApi, platform = Platform.iPad } = store.mutate.config || {};
@@ -100,7 +87,9 @@ export function setupClient() {
 		item.push({
 			name: event.sender.nickname,
 			id: event.message_id,
-			content: replaceImage(event.toString()),
+			content: JSON.stringify(
+				event.message.map((e) => (e.type === "text" ? e.text : e)),
+			),
 			timestamp: Date.now().toString(),
 		});
 	});
@@ -114,7 +103,9 @@ export function setupClient() {
 			id: event.message_id,
 			name: event.sender.card || event.sender.nickname,
 			groupName: event.group_name,
-			content: replaceImage(event.toString()),
+			content: JSON.stringify(
+				event.message.map((e) => (e.type === "text" ? e.text : e)),
+			),
 			timestamp: Date.now().toString(),
 		});
 	});
